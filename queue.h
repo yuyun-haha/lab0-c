f4c713e443fec27848be606486a4e1b3150ffb65
yuyun-haha <a23569314a@gmail.com>
2020-02-27 23:15:47 +0800

    Save file

--- queue.h
+++ queue.h
@@ -29,6 +29,7 @@ typedef struct {
      *        to efficiently implement q_size and q_insert_tail.
      */
     /* TODO: Remove the above comment when you are about to implement. */
+    int size;
 } queue_t;
 
 /* Operations on queue */
